fa8465876054d042f884916d9885bb6c4a4d0612
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-02-13 09:18:52 -0500

    Update setup-EMAIL.sh

--- Scripts/setup-EMAIL.sh
+++ Scripts/setup-EMAIL.sh
@@ -5,12 +5,15 @@ curl -X DELETE "http://localhost:30920/_component_template/logs-ti_abusech.malwa
 curl -X DELETE "http://localhost:30920/_component_template/logs-ti_abusech.malwarebazaar@package" -H "Content-Type: application/json" -u "sdg:changeme"
 
 # Recreate them in your image
-curl -X PUT "http://localhost:30920/_component_template/logs-ti_abusech.malware@package" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Component-Templates/logs-ti_abusech.malware.json
-curl -X PUT "http://localhost:30920/_index_template/logs-ti_abusech.malware" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Index-Templates/logs-ti_abusech.malware.json
-curl -X PUT "http://localhost:30920/_index_template/logs-email.filter" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Index-Templates/logs-email.filter.json
+curl -X PUT "http://localhost:30920/_component_template/logs-ti_abusech.malware@package" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGpy/Component-Templates/logs-ti_abusech.malware.json
+curl -X PUT "http://localhost:30920/_index_template/logs-ti_abusech.malware" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGpy/Index-Templates/logs-ti_abusech.malware.json
+curl -X PUT "http://localhost:30920/_index_template/logs-email.filter" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGpy/Index-Templates/logs-email.filter.json
 
 
 # Rollover existing index in order to inherit new settings without any garbage data
 #curl -X POST "http://localhost:30920/logs-windows.sysmon_operation-default/_rollover" -u "sdg:changeme"
 
-java -jar /root/SDGv2/build/libs/SDGv2-1.0.0-SNAPSHOT.jar /root/SDGv2/Tracks/saife-email.yml
+# Begin data generation
+python3 -m venv venv && source venv/bin/activate
+pip install -r /root/SDGpy/requirements.txt
+python /root/SDGpy/sdg.py /root/SDGpy/Tracks/saife-email.yml
