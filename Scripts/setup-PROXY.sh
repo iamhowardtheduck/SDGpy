cf5f4aa48ca41489c88ef270655131703c6e0942
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-02-13 09:19:33 -0500

    Update setup-PROXY.sh

--- Scripts/setup-PROXY.sh
+++ Scripts/setup-PROXY.sh
@@ -3,8 +3,11 @@ curl -X DELETE "http://localhost:30920/_index_template/logs-proxysg.log" -H "Con
 curl -X DELETE "http://localhost:30920/_component_template/logs-proxysg.log@package" -H "Content-Type: application/json" -u "sdg:changeme"
 
 # Recreate them in your image
-curl -X PUT "http://localhost:30920/_component_template/logs-proxysg.log@package" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Component-Templates/logs-proxysg.log.json
-curl -X PUT "http://localhost:30920/_index_template/logs-proxysg.log" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Index-Templates/logs-proxysg.log.json
+curl -X PUT "http://localhost:30920/_component_template/logs-proxysg.log@package" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGpy/Component-Templates/logs-proxysg.log.json
+curl -X PUT "http://localhost:30920/_index_template/logs-proxysg.log" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGpy/Index-Templates/logs-proxysg.log.json
 
 # Begin data generation
-java -jar /root/SDGv2/build/libs/SDGv2-1.0.0-SNAPSHOT.jar /root/SDGv2/Tracks/saife-proxy.yml
+# Begin data generation
+python3 -m venv venv && source venv/bin/activate
+pip install -r /root/SDGpy/requirements.txt
+python /root/SDGpy/sdg.py /root/SDGpy/Tracks/saife-proxy.yml
