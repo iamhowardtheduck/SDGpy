196cb0ab4e0db884c83b863232385123f900dc98
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-02-13 09:05:29 -0500

    Update setup_School_Day_Lesson2.sh

--- Scripts/setup_School_Day_Lesson2.sh
+++ Scripts/setup_School_Day_Lesson2.sh
@@ -33,4 +33,6 @@ curl -X PUT "http://localhost:30920/_index_template/logs-windows.sysmon_operatio
 
 
 # Begin data generation
-java -jar /root/SDGv2/build/libs/SDGv2-1.0.0-SNAPSHOT.jar /root/SDGv2/Tracks/schoolday_all_lesson2.yml
+python3 -m venv venv && source venv/bin/activate
+pip install -r /root/SDGpy/requirements.txt
+python /root/SDGpy/sdg.py /root/SDGpy/Tracks/schoolday_all_lesson2.yml
