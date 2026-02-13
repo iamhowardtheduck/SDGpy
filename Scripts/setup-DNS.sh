c1670291e35e1d071219b980809ad99089feb0cb
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-02-13 09:18:18 -0500

    Update setup-DNS.sh

--- Scripts/setup-DNS.sh
+++ Scripts/setup-DNS.sh
@@ -3,10 +3,11 @@ curl -X DELETE "http://localhost:30920/_index_template/logs-network_traffic.dns"
 curl -X DELETE "http://localhost:30920/_component_template/logs-network_traffic.dns@package" -H "Content-Type: application/json" -u "sdg:changeme"
 
 # Recreate them in your image
-curl -X PUT "http://localhost:30920/_component_template/logs-network_traffic.dns@package" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Component-Templates/logs-network_traffic.dns.json
-curl -X PUT "http://localhost:30920/_index_template/logs-network_traffic.dns" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGv2/Index-Templates/logs-network_traffic.dns.json
+curl -X PUT "http://localhost:30920/_component_template/logs-network_traffic.dns@package" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGpy/Component-Templates/logs-network_traffic.dns.json
+curl -X PUT "http://localhost:30920/_index_template/logs-network_traffic.dns" -H "Content-Type: application/json" -u "sdg:changeme" -d @/root/SDGpy/Index-Templates/logs-network_traffic.dns.json
 
-# Rollover existing index in order to inherit new settings without any garbage data
-#curl -X POST "http://localhost:30920/logs-windows.sysmon_operation-default/_rollover" -u "sdg:changeme"
 
-java -jar /root/SDGv2/build/libs/SDGv2-1.0.0-SNAPSHOT.jar /root/SDGv2/Tracks/saife-dns.yml
+# Begin data generation
+python3 -m venv venv && source venv/bin/activate
+pip install -r /root/SDGpy/requirements.txt
+python /root/SDGpy/sdg.py /root/SDGpy/Tracks/saife-dns.yml
