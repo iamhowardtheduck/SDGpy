291fb2fb20320d37f0522a8dfb5f42eadcfe3bb6
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-02-19 12:37:30 -0500

    Update setup_School_Day_Lesson3.sh

--- Scripts/setup_School_Day_Lesson3.sh
+++ Scripts/setup_School_Day_Lesson3.sh
@@ -1,4 +1,4 @@
 # Begin data generation
 python3 -m venv venv && source venv/bin/activate
 pip install -r /root/SDGpy/requirements.txt
-python /root/SDGpy/sdg.py /root/SDGpy/Tracks/schoolday_all_lesson3.yml
+python3 /root/SDGpy/sdg.py /root/SDGpy/Tracks/schoolday_all_lesson3.yml
