d2bf00edfb571a8273222647ef21b6b46d9c71b6
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-06-04 11:30:25 -0400

    Update setup-SAIFE.sh

--- Scripts/setup-SAIFE.sh
+++ Scripts/setup-SAIFE.sh
@@ -26,10 +26,10 @@ curl -X POST "http://localhost:30920/_security/user/sdg" -H "Content-Type: appli
 
 
 # Install LLM Connector
-bash /opt/workshops/elastic-llm.sh -m gpt-5.2 -k false -d true -n gpt5-connector -P curriculum-development
+bash /opt/workshops/elastic-llm.sh -m anthropic -k false -d true -n gpt5-connector -P curriculum-development
 
 echo
-echo "GPT-5.2 AI Connector configured as gpt5-connector"
+echo "Anthropic Connector configured as gpt5-connector"
 echo
 
 # Use Security view
