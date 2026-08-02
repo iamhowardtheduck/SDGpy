c3139a20c3600db2e92302ace6afebf556e5793b
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-08-01 20:05:42 -0400

    Update setup-SCHOOL_DAY-9-4.sh

--- Scripts/setup-SCHOOL_DAY-9-4.sh
+++ Scripts/setup-SCHOOL_DAY-9-4.sh
@@ -53,11 +53,11 @@ curl -X POST "http://localhost:30920/_security/user/sdg" -H "Content-Type: appli
 
 
 # Install LLM Connector
-bash /opt/workshops/elastic-llm.sh -m gpt-5.5 -k false -d true 
+#bash /opt/workshops/elastic-llm.sh -m gpt-5.5 -k false -d true 
 
-echo
-echo "OpenAI Connector configured"
-echo
+#echo
+#echo "OpenAI Connector configured"
+#echo
 
 # Use Security view
 bash /opt/workshops/elastic-view.sh -v classic
