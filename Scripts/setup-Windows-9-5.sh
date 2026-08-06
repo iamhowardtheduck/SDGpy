e39c8b9ddea42080aff556997284ceea50ce69a4
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-08-06 16:09:12 -0400

    Create setup-Windows-9-5.sh

---
+++ Scripts/setup-Windows-9-5.sh
@@ -0,0 +1,31 @@
+# DELETE Pre-Configured Windows templates
+curl -X DELETE "https://es.elastic.lab:443/_index_template/logs-netflow.log" -H "Content-Type: application/json" -u "sdg:changeme"
+curl -X DELETE "https://es.elastic.lab:443/_component_template/logs-netflow.log@package" -H "Content-Type: application/json" -u "sdg:changeme"
+curl -X DELETE "https://es.elastic.lab:443/_index_template/logs-windows.sysmon_operational" -H "Content-Type: application/json" -u "sdg:changeme"
+curl -X DELETE "https://es.elastic.lab:443/_component_template/logs-windows.sysmon_operational@package" -H "Content-Type: application/json" -u "sdg:changeme"
+
+# DELETE associated datastream if present
+#curl -X DELETE "https://es.elastic.lab:443//_data_stream/logs-windows.sysmon_operational-default" -u "sdg:changeme"
+
+# Create an updated ingest pipeline for the custom logs
+curl -X PUT "https://es.elastic.lab:443/_ingest/pipeline/logs@custom" -H "Content-Type: application/x-ndjson" -u "sdg:changeme" -d @/home/elastic/SDGpy/Ingest-Pipelines/logs@custom.json
+
+# Recreate them in your image
+curl -X PUT "https://es.elastic.lab:443/_component_template/logs-netflow.log@package" -H "Content-Type: application/json" -u "sdg:changeme" -d @/home/elastic/SDGpy/Component-Templates/logs-netflow.log.json
+curl -X PUT "https://es.elastic.lab:443/_index_template/logs-netflow.log" -H "Content-Type: application/json" -u "sdg:changeme" -d @/home/elastic/SDGpy/Index-Templates/logs-netflow.log.json
+curl -X PUT "https://es.elastic.lab:443/_component_template/logs-windows.sysmon_operational@package" -H "Content-Type: application/json" -u "sdg:changeme" -d @/home/elastic/SDGpy/Component-Templates/logs-windows.sysmon-operational.json
+curl -X PUT "https://es.elastic.lab:443/_index_template/logs-windows.sysmon_operational" -H "Content-Type: application/json" -u "sdg:changeme" -d @/home/elastic/SDGpy/Index-Templates/logs-windows.sysmon_operational.json
+curl -X PUT "https://es.elastic.lab:443/_component_template/logs@settings" -H "Content-Type: application/json" -u "sdg:changeme" -d @/home/elastic/SDGpy/Component-Templates/logs@settings.json
+curl -X PUT "https://es.elastic.lab:443/_index_template/logs" -H "Content-Type: application/json" -u "sdg:changeme" -d @/home/elastic/SDGpy/Index-Templates/logs.json
+
+# Create Datastream:
+curl -X PUT "https://es.elastic.lab:443/_data_stream/logs-windows.sysmon_operational-default" -H "Content-Type: application/json" -u "sdg:changeme"
+curl -X PUT "https://es.elastic.lab:443/_data_stream/logs-netflow.log-default" -H "Content-Type: application/json" -u "sdg:changeme"
+
+# Rollover existing index in order to inherit new settings without any garbage data
+#curl -X POST "https://es.elastic.lab:443/logs-windows.sysmon_operation-default/_rollover" -u "sdg:changeme"
+
+# Begin data generation
+python3 -m venv venv && source venv/bin/activate
+pip install -r /home/elastic/SDGpy/requirements.txt
+python3 /home/elastic/SDGpy/sdg.py /home/elastic/SDGpy/Tracks/saife-windows.yml
