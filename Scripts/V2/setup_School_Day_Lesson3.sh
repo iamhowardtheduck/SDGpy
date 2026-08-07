4e598ab0342e2b047b189ac3930448cf13b44d8d
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-08-07 08:01:14 -0400

    Create setup_School_Day_Lesson3.sh

---
+++ Scripts/V2/setup_School_Day_Lesson3.sh
@@ -0,0 +1,4 @@
+# Begin data generation
+python3 -m venv venv && source venv/bin/activate
+pip install -r /home/elastic/SDGpy/requirements.txt
+python3 /home/elastic/SDGpy/sdg.py /home/elastic/SDGpy/Tracks/V2/schoolday_all_lesson3.yml
