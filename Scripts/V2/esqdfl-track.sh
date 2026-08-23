954e33e2725a2fb8e851559054417524671f32ee
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-08-23 08:10:44 -0400

    Update esqdfl-track.sh

--- Scripts/V2/esqdfl-track.sh
+++ Scripts/V2/esqdfl-track.sh
@@ -1,4 +1,4 @@
 # Begin data generation
 python3 -m venv venv && source venv/bin/activate
-pip install -r /root/SDGpy/requirements.txt
-python3 /root/SDGpy/sdg.py /root/SDGpy/Tracks/V2/esqldf.yml
+pip install -r /home/elastic/SDGpy/requirements.txt
+python3 /home/elastic/SDGpy/sdg.py /home/elastic/SDGpy/Tracks/V2/esqldf.yml
