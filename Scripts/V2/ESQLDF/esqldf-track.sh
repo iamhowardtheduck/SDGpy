b9bd60d2edad52b6dea1c92f2c556bfbc6eaa06d
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-08-25 10:25:20 -0400

    Update esqldf-track.sh

--- Scripts/V2/ESQLDF/esqldf-track.sh
+++ Scripts/V2/ESQLDF/esqldf-track.sh
@@ -11,4 +11,4 @@ fi
 
 python3 -m venv venv && source venv/bin/activate
 pip install -r /home/elastic/SDGpy/requirements.txt
-python3 /home/elastic/SDGpy/sdg.py /home/elastic/SDGpy/Tracks/V2/esqldf.yml
+python3 /home/elastic/SDGpy/sdg.py /home/elastic/SDGpy/Tracks/V2/esqldf-V2.yml
